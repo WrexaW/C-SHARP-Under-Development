c182ca2a8eac3dd7b68972095d595443e4671b79
armin ㅤㅤ <xboxrexax@gmail.com>
2024-11-23 19:21:42 +0330

    test 1

--- Entities/UserEntity.cs
+++ Entities/UserEntity.cs
@@ -11,6 +11,7 @@ namespace WebApplication_C14.Entities
         public string Mobile { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public int MyProperty { get; set; }
 
     }
 }
