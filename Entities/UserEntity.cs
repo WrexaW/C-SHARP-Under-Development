94076f4bc51242aaf45e9e78fcf7a2c292133539
armin ㅤㅤ <xboxrexax@gmail.com>
2024-11-23 19:26:26 +0330

    Revert "test 1"
    This reverts commit c182ca2a8eac3dd7b68972095d595443e4671b79.

--- Entities/UserEntity.cs
+++ Entities/UserEntity.cs
@@ -11,7 +11,6 @@ namespace WebApplication_C14.Entities
         public string Mobile { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public int MyProperty { get; set; }
 
     }
 }
